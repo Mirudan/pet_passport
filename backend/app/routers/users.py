f575bab162c3078d570c49cf814065ebd90d9137
Alexei Voropaev <chaos.73@yandex.ru>
2025-04-30 20:29:49 +0400

    добавил роутеров для работы с пользователями (удаление и просмотр всех пользователей)

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.models import User
 from app.schemas.users import UserCreate, UserResponse
+from sqlalchemy import select
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -10,8 +11,10 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post("/", response_model=UserResponse)
 async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     # Проверяем, есть ли пользователь в БД
-    existing_user = await db.execute(User).where(User.telegram_id == user.telegram_id)
-    if existing_user.scalar():
+    result = await db.execute(select(User).where(User.telegram_id == user.telegram_id))
+    existing_user = result.scalar()
+
+    if existing_user:
         raise HTTPException(400, "User already exists")
 
     # Создаем нового пользователя
@@ -24,8 +27,27 @@ async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
 
 @router.get("/{telegram_id}", response_model=UserResponse)
 async def get_user(telegram_id: int, db: AsyncSession = Depends(get_db)):
-    user = await db.execute(User).where(User.telegram_id == telegram_id)
+    user = await db.execute(select(User).where(User.telegram_id == telegram_id))
     user = user.scalar()
     if not user:
         raise HTTPException(404, "User not found")
     return user
+
+
+@router.get("/", response_model=list[UserResponse])
+async def get_all_users(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User))
+    users = result.scalars().all()
+    return users
+
+
+@router.delete("/{telegram_id}")
+async def delete_user(telegram_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User).where(User.telegram_id == telegram_id))
+    user = result.scalar()
+    if not user:
+        raise HTTPException(404, "User not found")
+
+    await db.delete(user)
+    await db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
